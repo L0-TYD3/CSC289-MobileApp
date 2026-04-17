9f3c5c5286300521f05b4f354ccc8c5582706b40
= <=>
2026-04-17 16:01:01 -0400

    feat: enhance account screen with logout functionality and improved styling
    - Added logout button to the AccountScreen for user session management.
    - Updated styling for consistency in the AccountScreen layout.
    - Improved error handling and loading indicators for better user experience.

--- features/account/components/AccountScreen.tsx
+++ features/account/components/AccountScreen.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 import { useGetCustomer } from '@/features/account/hooks/useGetCustomer';
+import { useAuthStore } from '@/features/auth/store';
 import { useRouter } from 'expo-router';
 import { ActivityIndicator, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,23 +9,24 @@ import { AccountCard } from './AccountCard';
 import { MemberCard } from './MemberCard';
 
 export default function AccountScreen() {
+  const logout = useAuthStore((state) => state.logout);
   const { data: customer, isPending, isError } = useGetCustomer();
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 bg-background">
-      <View className="flex-1 px-4 pt-6 gap-4">
-        <Text variant="h3">My Account</Text>
+    <SafeAreaView className='flex-1 bg-background'>
+      <View className='flex-1 px-4 pt-6 gap-4'>
+        <Text variant='h3'>My Account</Text>
 
         {isPending && (
-          <View className="flex-1 items-center justify-center">
+          <View className='flex-1 items-center justify-center'>
             <ActivityIndicator />
           </View>
         )}
 
         {isError && (
-          <View className="flex-1 items-center justify-center">
-            <Text className="text-destructive text-sm">
+          <View className='flex-1 items-center justify-center'>
+            <Text className='text-destructive text-sm'>
               Failed to load account details. Please try again.
             </Text>
           </View>
@@ -32,8 +34,23 @@ export default function AccountScreen() {
 
         {customer && <AccountCard customer={customer} />}
         {customer && <MemberCard customer={customer} />}
-        {customer && <Button variant="outline" onPress={() => router.push('/addresses')}> Addresses</Button>}
-        
+        {customer && (
+          <Button
+            variant='outline'
+            onPress={() => router.push('/addresses')}
+          >
+            {' '}
+            Addresses
+          </Button>
+        )}
+      </View>
+      <View className='px-4'>
+        <Button
+          variant='default'
+          onPress={() => logout()}
+        >
+          Logout
+        </Button>
       </View>
     </SafeAreaView>
   );
